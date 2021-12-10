fee01b2e5c18f1910c4bcac62458a6c46b59b618
sudhan6 <rbsudhan2001@gmail.com>
2021-12-10 19:50:36 +0530

    test

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use("/api/v1/product", productRoutes)
 
 app.get('/',(req,res) =>
 {
-        res.send(`server Listening to  ${port}`)
+        res.send(`server Listening to  ${port}(Deployed from workflow)`)
 })
 
 app.listen(port,(req,res) => {
